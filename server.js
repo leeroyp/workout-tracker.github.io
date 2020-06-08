6e8a6b9f4851981ad797c950da7a492a5af493f2
Leeroy Phili <leeroyphili@gmail.com>
2020-06-08 15:45:37 -0400

    created apit routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const mongoose = require("morgan");
+const morgan = require("morgan");
 
 
 const PORT = process.env.PORT || 3000;
